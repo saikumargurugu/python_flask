8e1ad8c0087cfeb7121f91dd6598b11b2fef84ec
sai kumar <47783993+saikumargurugu@users.noreply.github.com>
2019-09-23 18:16:02 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@ import psycopg2
 from flask import Flask,url_for,render_template, request, redirect, flash,session,g
 from forms import AddDataForm,RemoveUser,UpdateUser
 import os
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/home/prakash/Desktop/userdata/usimg'
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 SECRET_KEY = os.urandom(32)
@@ -12,7 +14,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 #app.config['SECRET-KEY']='dd916e7b012cdd2b8ed48d32f35fc9ee'
 
 connection = psycopg2.connect(user="postgres",
-                            password="sai81432021",
+                            password="unosimple",
                             host="localhost",
                             port="5432",
                             database="userinfo")
@@ -28,23 +30,38 @@ def before_request():
 def login():
    if request.method== 'POST':
       session.pop('users', None)
-      cursor.execute( """ SELECT phno FROM userdataa WHERE sno = %s;""" , request.form['username'])
-      d=cursor.fetchall()
-      connection.commit()
-      k=list(d[0])
-      if int(request.form['password']) in k:
-         print(request.form['password'])
-         session['users']=request.form['username']
-         return redirect(url_for('hello_name'))
+      f=int(request.form['username'])
+      cursor.execute( """ SELECT phno FROM userdataa WHERE  sno= (%s) ;""" ,[f])
+      try:
+         d=cursor.fetchall()
+         connection.commit()
+         k=list(d[0])
+         print(type(int(request.form['password'])))
+         print(type(k[0]))
+         if int(request.form['password']) == k[0]:
+            print(request.form['password'])
+            session['users']=request.form['username'] 
+            return redirect(url_for('hello_name'))
+         else:
+            flash(f'Please enter a valid   user name and password')
+      except:
+            flash(f'incorrect moblile number, plese make sure that lenth is correct ', 'success')
    return render_template('login.html')
 
-
+@app.route('/logout', methods=['GET'])   
+def logout():
+   if 'users' in session:  
+      session.pop('users',None)  
+      return redirect(url_for('login'))  
+   else:  
+      flash(f'you need to  login to logout')
+      return redirect(url_for('login')) 
 
 @app.route('/hello', methods=['GET','POST'])
 def hello_name():
    if g.users:
       form= RemoveUser()
-      cursor.execute("""select * FROM userdataa""")
+      cursor.execute("""select * FROM userdataa ORDER BY sno ASC """)
       d=cursor.fetchall()
       connection.commit()
       l=[]
@@ -55,13 +72,11 @@ def hello_name():
    else:
       return redirect(url_for('login'))
 
-
-
-
 @app.route('/adddata', methods=['GET','POST'])
 def adddata():
-   form = AddDataForm()
-   if request.method =='POST':
+   if g.users:
+      form = AddDataForm()
+      if request.method =='POST':
          uname=request.form['uname']
          phno=request.form['phno']
          email=request.form['email']
@@ -81,39 +96,61 @@ def adddata():
                flash(f'email domain error (.) {email} ', 'success')
          else:
             flash(f'email domain error _@_ {email} ', 'success')
-   return render_template('add.html',form=form)
+      return render_template('add.html',form=form)
+   else:
+      return redirect(url_for('login'))
 
 @app.route('/upuser/<string:id>', methods=['GET','POST'])
 def updatedata(id):
-   form = UpdateUser()
-   if request.method =='POST':
-      uname=request.form['uname']
-      phno=request.form['phno']
-      email=request.form['email']
-      if request.form['uname'] :
-         sql = """ UPDATE userdataa SET uname = (%s) WHERE sno = (%s)"""
-         cursor.execute(sql,(str(uname),id ))
-         connection.commit()
-      if request.form['phno'] :
-         sql = """ UPDATE userdataa SET phno = (%s) WHERE sno = (%s)"""
-         cursor.execute(sql,(str(phno),id ))
-         connection.commit()
-      if request.form['email'] :
-         sql = """ UPDATE userdataa SET email = (%s) WHERE sno = (%s)"""
-         cursor.execute(sql,(str(email),id ))
-         connection.commit()
-      return redirect(url_for('hello_name'))
-
+   if g.users:
+      form = AddDataForm()
+      form = UpdateUser()
+      if request.method =='POST':
+         uname=request.form['uname']
+         phno=request.form['phno']
+         email=request.form['email']
+         if request.form['uname'] :
+            sql = """ UPDATE userdataa SET uname = (%s) WHERE sno = (%s)"""
+            cursor.execute(sql,(str(uname),id ))
+            connection.commit()
+         if request.form['phno'] :
+            sql = """ UPDATE userdataa SET phno = (%s) WHERE sno = (%s)"""
+            cursor.execute(sql,(str(phno),id ))
+            connection.commit()
+         if request.form['email'] :
+            sql = """ UPDATE userdataa SET email = (%s) WHERE sno = (%s)"""
+            cursor.execute(sql,(str(email),id ))
+            connection.commit()
+         return redirect(url_for('hello_name'))
+   else:
+      return redirect(url_for('login'))
 
    return render_template('update.html',form=form)
-@app.route('/delet/<string:id>', methods=['GET','POST'])
+@app.route('/delet/<string:id>', methods=['GET'])
 def remove_user(id):
-   print(id)
-   postgreSQL_select_Query="""DELETE  FROM userdataa WHERE sno = %s"""
-   cursor.execute(postgreSQL_select_Query,id)
-   connection.commit()
-   return redirect(url_for('hello_name'))
-  
+   if g.users: 
+      k=int(id)
+      print(id)
+      postgreSQL_select_Query="""DELETE  FROM userdataa WHERE sno = %s"""
+      cursor.execute(postgreSQL_select_Query,[k])
+      connection.commit()
+      return redirect(url_for('hello_name'))
+   else:
+      return redirect(url_for('login'))
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      if 'file' not in request.files[file]:
+         flash('No file part')
+         return redirect(url_for('hello_name'))
+      if 'file' in request.files[file]:
+         filer= secure_filename(request.files['file'])
+         file.save(os.path.join(app.config['UPLOAD_FOLDER'],filer))
+         flash('file uploaded successfully')
+         return redirect(url_for('hello_name', file=file))
+   return render_template('index.hrml')
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
