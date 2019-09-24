a8abb545d4fdb981b47d5ee23e6236fa807704ce
sai kumar <47783993+saikumargurugu@users.noreply.github.com>
2019-09-24 11:06:08 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -3,8 +3,6 @@ from wtforms import StringField,SubmitField, IntegerField
 from wtforms.fields.html5 import TelField
 from wtforms.validators import DataRequired, Length,Email
 class AddDataForm(FlaskForm):
-
-
     uname=StringField('uname', validators=[DataRequired(),Length(min=2, max=20)])
     email=StringField('email', validators=[DataRequired(),Email()])
     phno= IntegerField('phno',validators=[DataRequired(),Length(10)])
